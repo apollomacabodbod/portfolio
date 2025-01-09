504ccee5c774513f6c5c07c3d671bf8807697312
apollo <apollomacabodbod8@gmail.com>
2025-01-09 20:30:42 +0800

    remove max width in text

--- src/home/_components/home-hero-section.tsx
+++ src/home/_components/home-hero-section.tsx
@@ -8,12 +8,37 @@ export default function HomeHeroSection(){
   return(<>
 
 
-    <div className="flex flex-col my-[10.5em] border border-[#50B498] green"  >
+    <div className="flex flex-col my-[9em] border border-[#50B498] green "  >
 
 
       <p className="text-[#030712] font-inter-tight text-[3.4rem] sm:text-[6.4rem] not-italic font-bold leading-[1em] ">I’m a frontend developer with passion to create <br></br><span className="text-[#AEB2BA]"> a great experiences</span> </p>
 
 
+
+
+      <div className="lg:flex lg:items-center lg:justify-between lg:gap-[2em] lg:mt-[2.5em] ">
+
+
+
+        <div className="flex flex-col mt-[2.5em] lg:mt-[0em]">
+
+          <img src="/home-hero-section-divider.svg" alt="" className=""/>
+
+        </div>
+
+
+
+        <div className="flex flex-col">
+
+           
+          <p className="text-[#374151] font-inter-tight text-[1.375rem] not-italic font-normal mt-[1em] lg:mt-[0em] ">I'm Apollo, a frontend developer I specialize in creating intuitive and engaging digital experiences. and building user-friendly interfaces seamlessly.</p>
+           
+
+        </div>
+
+
+      
+      </div>
 
     </div>
     
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
 
 
-    <div className="flex flex-col max-w-[1200px] lg:mx-auto px-[1.25em]">
+    <div className="flex flex-col max-w-[1200px] lg:mx-auto px-[1.25em] border border-[#50B498] green">
 
       <HomeHeroSection/>
 
