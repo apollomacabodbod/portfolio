866c76750c5da525176590cba14a64028ec52044
apollo <apollomacabodbod8@gmail.com>
2025-01-10 12:42:01 +0800

    added new latest works cards

---
+++ src/home/_components/home-latest-works-cards.tsx
@@ -0,0 +1,189 @@
+
+
+
+export default function HomeLatestWorksCards(){
+
+  return(<>
+
+    <div className="grid custom-760:grid-cols-2 mt-[3.5em] gap-[2em] mb-[3.5em]">
+
+
+      <div className="flex flex-col ">
+
+
+        <div className="flex items-center cursor-pointer">
+
+          <img src="film.png" alt="" className="w-full object-cover object-top rounded-[0.25em]"/>
+        </div>
+
+
+
+
+        <div className="flex flex-col mt-[1.5em] border border-[#50B498] green">
+
+
+          <div className="flex items-center justify-between">
+
+
+
+            <div className="flex flex-col">
+
+
+              <p className="text-[#030712] font-inter-tight text-[1.3rem] not-italic font-semibold ">Filmina</p>
+
+
+              <div className="flex items-center gap-[0.5em]">
+                <p className="text-[#374151] font-inter-tight text-[1.125rem] not-italic font-normal ">Tech Stack</p>
+
+                <div className="flex items-center gap-[0.4em]">
+
+                  <img src="/JavaScript-logo.png" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+                  <img src="/logo512.png" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+                  <img src="/html5.png" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+                  <img src="/Tailwind_CSS_Logo.svg" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+
+
+                </div>
+
+
+              </div>
+                  
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+            <div className="flex item-center py-[1em] px-[1.75em]  bg-[#030712] rounded-[6.25em] cursor-pointer gap-[0.5em]">
+
+              <img src="/home-latest-works-arrow.svg" alt=""  />
+
+
+
+
+
+
+
+
+
+            </div>
+
+                    
+
+
+                  
+
+          </div>
+
+
+        </div>
+
+
+
+
+
+      </div>
+
+
+
+
+
+      <div className="flex flex-col ">
+
+
+        <div className="flex items-center cursor-pointer">
+
+          <img src="parking.png" alt="" className="w-full object-cover object-top rounded-[0.25em]"/>
+        </div>
+
+
+
+
+        <div className="flex flex-col mt-[1.5em] border border-[#50B498] green">
+
+
+          <div className="flex items-center justify-between">
+
+
+
+            <div className="flex flex-col">
+
+
+              <p className="text-[#030712] font-inter-tight text-[1.3rem] not-italic font-semibold ">Park Camping</p>
+
+
+              <div className="flex items-center gap-[0.5em]">
+                <p className="text-[#374151] font-inter-tight text-[1.125rem] not-italic font-normal ">Tech Stack</p>
+
+                <div className="flex items-center gap-[0.4em]">
+
+                  <img src="/JavaScript-logo.png" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+                  <img src="/logo512.png" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+                  <img src="/html5.png" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+                  <img src="/Tailwind_CSS_Logo.svg" alt="" className="w-[1.5em] object-cover rounded-[0.25em]"/>
+
+
+                </div>
+
+
+              </div>
+                  
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+            <div className="flex item-center py-[1em] px-[1.75em]  bg-[#030712] rounded-[6.25em] cursor-pointer gap-[0.5em]">
+
+              <img src="/home-latest-works-arrow.svg" alt=""  />
+
+
+
+
+
+
+
+
+
+            </div>
+
+                    
+
+
+                  
+
+          </div>
+
+
+        </div>
+
+
+
+
+
+      </div>
+
+
+
+    </div>
+    
+    
+  </>)
+}
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,6 +1,7 @@
 import HomeAboutMe from "./_components/home-about-me";
 import HomeHeroSection from "./_components/home-hero-section";
 import HomeLatestWorks from "./_components/home-latest-works";
+import HomeLatestWorksCards from "./_components/home-latest-works-cards";
 
 export default function Home() {
 
@@ -14,6 +15,7 @@ export default function Home() {
       <HomeHeroSection/>
       <HomeAboutMe/>
       <HomeLatestWorks/>
+      <HomeLatestWorksCards/>
 
     </div>
 
