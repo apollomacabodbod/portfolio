85e263b6e9e10d09322b58b0bc7525ff7bbedf2a
apollo <apollomacabodbod8@gmail.com>
2025-01-10 15:24:59 +0800

    added home links

---
+++ src/home/_components/home-links.tsx
@@ -0,0 +1,68 @@
+
+import { Link } from "react-router-dom"
+
+export default function HomeLinks(){
+
+
+
+  const handleClickLinkedIn = () => {
+    window.open('https://www.linkedin.com/in/apollo-macabodbod-832a8a279/');
+  };
+
+
+
+  const handleClickGithub = () => {
+    window.open('https://github.com/apollomacabodbod');
+  };
+
+
+  return(<>
+
+    <div className="flex flex-col my-[5.5em] ">
+
+
+
+
+
+
+      <div className="flex items-center gap-[3.5em]" >
+
+           
+            
+            
+
+        <div className="flex items-center gap-[0.75em] cursor-pointer" onClick={handleClickLinkedIn}>
+
+       
+
+          <p className="text-[#030712] font-inter-tight text-[1rem] not-italic font-medium uppercase">LinkedIn</p>
+
+          <img src="/home-links.svg" alt=""  className="transform transition-transform duration-300 hover:scale-110"/>
+
+
+        </div>
+
+
+        <div className="flex items-center gap-[0.75em] cursor-pointer" onClick={handleClickGithub}>
+
+          <p className="text-[#030712] font-inter-tight text-[1rem] not-italic font-medium uppercase">github</p>
+
+          <img src="/home-links.svg" alt="" className="transform transition-transform duration-300 hover:scale-110"/>
+
+
+        </div>
+
+
+
+
+      </div>
+
+     
+
+
+
+    </div>
+    
+    
+  </>)
+}
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -2,6 +2,7 @@ import HomeAboutMe from "./_components/home-about-me";
 import HomeHeroSection from "./_components/home-hero-section";
 import HomeLatestWorks from "./_components/home-latest-works";
 import HomeLatestWorksCards from "./_components/home-latest-works-cards";
+import HomeLinks from "./_components/home-links";
 
 export default function Home() {
 
@@ -13,6 +14,7 @@ export default function Home() {
     <div className="flex flex-col max-w-[1200px] lg:mx-auto px-[1.25em] border border-[#50B498] green">
 
       <HomeHeroSection/>
+      <HomeLinks/>
       <HomeAboutMe/>
       <HomeLatestWorks/>
       <HomeLatestWorksCards/>
