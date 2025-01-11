c8e65fe6c0de6b8fae38a05933093991e1e214df
apollo <apollomacabodbod8@gmail.com>
2025-01-12 07:00:55 +0800

    added smooth transition

--- src/contact/_components/contact-submit.tsx
+++ src/contact/_components/contact-submit.tsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom"
-
+import { motion } from "framer-motion"
 
 export default function ContactSubmit(){
 
@@ -9,26 +9,50 @@ export default function ContactSubmit(){
   return(<>
 
 
-
-    <div className="flex items-center justify-center mb-[3em] transition-opacity duration-1000 ease-in-out active:scale-95 active:transition-transform active:duration-200 active:ease-out">
-
-
-      <Link to={"/contact"} className="flex items-center gap-[0.75em] bg-[#030712] py-[0.75em] px-[1.25em] rounded-[6.25em] cursor-pointer">
-                    
-                    
-        
-        <p className="text-white font-inter-tight  not-italic font-semibold text-[1rem] ">Message</p>
-        
-        <img src="/home-header-arrow.svg" alt="" />
-        
-        
-        
-      </Link>
+    <motion.div className="flex flex-col transition-all duration-1000 ease-in-out"
 
 
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.5 }}
+      variants={{
+        hidden: { opacity: 0, y: 10 },
+        visible: { 
+          opacity: 1, 
+          y: 0,
+          transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+        },
+      }}
 
-    </div>
-    
+
+
+    >
+
+
+      <div className="flex items-center justify-center mb-[3em] transition-opacity duration-1000 ease-in-out active:scale-95 active:transition-transform active:duration-200 active:ease-out">
+
+
+        <Link to={"/contact"} className="flex items-center gap-[0.75em] bg-[#030712] py-[0.75em] px-[1.25em] rounded-[6.25em] cursor-pointer">
+              
+              
+  
+          <p className="text-white font-inter-tight  not-italic font-semibold text-[1rem] ">Message</p>
+  
+          <img src="/home-header-arrow.svg" alt="" />
+  
+  
+  
+        </Link>
+
+
+
+      </div>
+
+
+    </motion.div>
+
+
+  
     
     
   </>)
