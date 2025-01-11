061b519048d552a4f2d76967005ebec39a61ed54
apollo <apollomacabodbod8@gmail.com>
2025-01-12 06:55:15 +0800

    remove unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {  Routes, Route} from "react-router-dom";
 import Home from "./home/page";
-import About from "./about/page";
 import Contact from "./contact/page";
 import Header from "./components/kit/header";
 import { LenisProvider } from "./context/smooth-scroll";
