126973de21d4667746e3b1c1bff55ae7b3dcdd52
apollo <apollomacabodbod8@gmail.com>
2025-01-11 11:39:21 +0800

    added smooth animation

--- src/components/kit/footer.tsx
+++ src/components/kit/footer.tsx
@@ -20,9 +20,41 @@ export default function Footer(){
 
 
       
-          <p className=" text-center text-[#4B5563] font-inter-tight text-[1rem]  font-normal not-italic mt-[0.5em]">"In battle, plans are useless, but planning is indispensible." - Dwight D. Eisenhower</p>
+          <motion.p className=" text-center text-[#4B5563] font-inter-tight text-[1rem]  font-normal not-italic mt-[0.5em] transition-all duration-1000 ease-in-out"
+          
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: { 
+                opacity: 1, 
+                y: 0,
+                transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+              },
+            }}
+          
+          
+          >"In battle, plans are useless, but planning is indispensible." - Dwight D. Eisenhower</motion.p>
 
-          <div className="flex items-center justify-center gap-[1em] mt-[1em]">
+          <motion.div className="flex items-center justify-center gap-[1em] mt-[1em] transition-all duration-1000 ease-in-out"
+          
+          
+          
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 10 },
+              visible: { 
+                opacity: 1, 
+                y: 0,
+                transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+              },
+            }}
+
+          
+          >
 
 
          
@@ -30,7 +62,7 @@ export default function Footer(){
             <img src="/footer-linkedIn.svg" alt="" />
             <img src="/footer-github.svg" alt="" />
 
-          </div>
+          </motion.div>
 
 
   
@@ -50,14 +82,45 @@ export default function Footer(){
 
             <div className="flex flex-col">
 
-              <p className=" text-center text-[#4B5563] font-inter-tight text-[1rem]  font-normal not-italic mt-[0.5em]">apollomacabodbod8 <span className="text-white font-semibold"> @gmail.com</span></p>
+              <motion.p className=" text-center text-[#4B5563] font-inter-tight text-[1rem]  font-normal not-italic mt-[0.5em] transtion-all duration-1000 ease-in-out"
+              
+              
+                  
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: { 
+                    opacity: 1, 
+                    y: 0,
+                    transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+                  },
+                }}
+              
+              >apollomacabodbod8 <span className="text-white font-semibold"> @gmail.com</span></motion.p>
 
             </div>
 
 
             <div className="flex flex-col">
 
-              <p className=" text-center text-[#4B5563] font-inter-tight text-[1rem]  font-normal not-italic mt-[0.5em]">© 2025 All Rights Reserved.</p>
+              <motion.p className=" text-center text-[#4B5563] font-inter-tight text-[1rem]  font-normal not-italic mt-[0.5em] transition-all duration-1000 ease-in-out"
+              
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={{
+                  hidden: { opacity: 0, y: 10 },
+                  visible: { 
+                    opacity: 1, 
+                    y: 0,
+                    transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+                  },
+                }}
+              
+              
+              >© 2025 All Rights Reserved.</motion.p>
 
             </div>
 
