29a5a8bede252cfadca23a18245d50f85bb2d9c1
apollo <apollomacabodbod8@gmail.com>
2025-01-12 13:35:37 +0800

    added emailjs in sending message

---
+++ src/contact/_components/contact-form.tsx
@@ -0,0 +1,166 @@
+
+
+import { motion } from "framer-motion";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+
+export default function ContactForm() {
+
+
+
+
+  // Explicitly typing the ref
+  const form = useRef<HTMLFormElement | null>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm("service_hgc1qto", "template_5yowsno", form.current, {
+          publicKey: "gNJPX-wgqHkkqtiWB",
+        })
+        .then(
+          () => {
+            console.log("SUCCESS!");
+            alert("Successfully send")
+              
+            form.current?.reset(); // Clear all fields
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    }
+  };
+
+
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+
+      <form className="flex flex-col w-full p-[2em] " ref={form} onSubmit={sendEmail}>
+            
+        <motion.p
+          className="text-center text-[#333] font-inter-tight text-[1rem] font-semibold transition-all duration-1000 ease-in-out"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 0, ease: "easeInOut" },
+            },
+          }}
+        >
+            Send a Message
+        </motion.p>
+  
+        <div className="flex items-center justify-between gap-[1em] mt-4">
+          <div className="flex flex-col flex-1">
+            <motion.input
+              name="from_name"
+              type="text"
+              className="w-full outline-none border-b-2 border-[rgba(16,34,29,0.60)] py-[1em] text-[rgba(16,34,29,0.60)] font-inter-tight text-[0.9375rem] not-italic font-normal transition-all duration-1000 ease-in-out"
+              placeholder="Your Name"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: { 
+                  opacity: 1, 
+                  y: 0,
+                  transition: { duration: 0, ease: "easeInOut" },
+                },
+              }}
+            />
+          </div>
+  
+          <div className="flex flex-col flex-1">
+            <motion.input
+              type="email"
+              name="user_email"
+              className="w-full outline-none border-b-2 border-[rgba(16,34,29,0.60)] py-[1em] text-[rgba(16,34,29,0.60)] font-inter-tight text-[0.9375rem] not-italic font-normal transition-all duration-1000 ease-in-out"
+              placeholder="Your Email"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, y: 10 },
+                visible: { 
+                  opacity: 1, 
+                  y: 0,
+                  transition: { duration: 0, ease: "easeInOut" },
+                },
+              }}
+            />
+          </div>
+        </div>
+  
+        <motion.textarea
+          name="message"
+          id=""
+          className="outline-none border-b-2 border-[rgba(16,34,29,0.60)] pt-[1em] pb-[4em] mt-4 text-[rgba(16,34,29,0.60)] font-inter-tight text-[0.9375rem] not-italic font-normal resize-none transition-all duration-1000 ease-in-out"
+          placeholder="Message"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 0, ease: "easeInOut" },
+            },
+          }}
+
+
+        ></motion.textarea>
+
+
+        <motion.div className="flex flex-col transition-all duration-1000 ease-in-out items-center justify-center flex-grow"
+
+
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+            },
+          }}
+
+
+
+        >
+
+
+          <div className="flex items-center justify-center mt-[2em] mb-[3em] transition-opacity duration-1000 ease-in-out active:scale-95 active:transition-transform active:duration-200 active:ease-out">
+
+
+            <button className="flex items-center gap-[0.75em] bg-[#030712] py-[0.75em] px-[1.25em] rounded-[6.25em] cursor-pointer" type="submit" >
+              
+              
+  
+              <p className="text-white font-inter-tight  not-italic font-semibold text-[1rem] ">Message</p>
+      
+              <img src="/home-header-arrow.svg" alt="" />
+      
+      
+      
+            </button >
+          </div>
+        </motion.div>
+
+
+      </form>
+    </div>
+  );
+}
+  
--- src/contact/page.tsx
+++ src/contact/page.tsx
@@ -1,5 +1,6 @@
 import ContactMessage from "./_components/contac-message";
 import ContactDetails from "./_components/contact-details";
+import Contactform from "./_components/contact-form";
 import ContactSubmit from "./_components/contact-submit";
 
 
@@ -11,9 +12,10 @@ export default function Contact() {
 
       <div className="flex flex-col min-h-screen max-w-[1200px] lg:mx-auto px-[1.25em]">
 
-        <ContactMessage/>
-        <ContactDetails/>
-        <ContactSubmit/>
+        {/* <ContactMessage/> */}
+        {/* <ContactDetails/>
+        <ContactSubmit/> */}
+        <Contactform/>
       
       
       
