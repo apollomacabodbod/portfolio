4da2f5919937a1b8696eb98fd477b3930ad6254f
apollo <apollomacabodbod8@gmail.com>
2025-01-12 08:29:56 +0800

    remove route in link submit

--- src/contact/_components/contact-submit.tsx
+++ src/contact/_components/contact-submit.tsx
@@ -32,7 +32,7 @@ export default function ContactSubmit(){
       <div className="flex items-center justify-center mb-[3em] transition-opacity duration-1000 ease-in-out active:scale-95 active:transition-transform active:duration-200 active:ease-out">
 
 
-        <Link to={"/contact"} className="flex items-center gap-[0.75em] bg-[#030712] py-[0.75em] px-[1.25em] rounded-[6.25em] cursor-pointer">
+        <Link to={""} className="flex items-center gap-[0.75em] bg-[#030712] py-[0.75em] px-[1.25em] rounded-[6.25em] cursor-pointer">
               
               
   
