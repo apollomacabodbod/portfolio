7b8d2f22b80083ac131dead3d2b34a613040df08
apollo <apollomacabodbod8@gmail.com>
2025-01-12 08:09:14 +0800

    added smooth transition

--- src/components/kit/not-found.tsx
+++ src/components/kit/not-found.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { motion } from "framer-motion";
 
 
 
@@ -20,16 +20,68 @@ export default function NotFound(){
 
 
 
-        <p className="text-center text-[#030712] font-inter-tight text-[4.5rem] not-italic font-bold leading-[1em]">404</p>
+        <motion.p className="text-center text-[#030712] font-inter-tight text-[4.5rem] not-italic font-bold leading-[1em] transition-all duration-1000 ease-in-out"
+        
+        
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+            },
+          }}
+        
+        
+        >404</motion.p>
 
 
-        <p className="text-[#030712] font-inter-tight text-[0.9375rem] not-italic font-normal text-center mt-[2em]">Somethings gone missing. Sorry, we couldn’t find the page you are looking for.</p>
+        <motion.p className="text-[#030712] font-inter-tight text-[0.9375rem] not-italic font-normal text-center mt-[2em] transition-all duration-1000 ease-in-out"
+        
+        
+        
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+            },
+          }}
+        
+        
+        >Somethings gone missing. Sorry, we couldn’t find the page you are looking for.</motion.p>
 
 
 
             
 
-        <div className="flex items-center justify-center mt-[2em]">
+        <motion.div className="flex items-center justify-center mt-[2em] transition-all duration-1000 ease-in-out"
+        
+        
+         
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 10 },
+            visible: { 
+              opacity: 1, 
+              y: 0,
+              transition: { duration: 0, ease: "easeInOut" }, // Applying duration and easing
+            },
+          }}
+        
+        
+        
+        >
 
           <Link to={"/"} className="flex items-center gap-[0.75em] bg-[#030712] py-[0.75em] px-[1.25em] rounded-[6.25em] cursor-pointer transition-opacity duration-1000 ease-in-out active:scale-95 active:transition-transform active:duration-200 active:ease-out ">
             
@@ -44,7 +96,7 @@ export default function NotFound(){
 
           </Link>
     
-        </div>
+        </motion.div>
 
 
 
