480cb6dcd055e51d91b05eaf65a6e3cca4031134
apollo <apollomacabodbod8@gmail.com>
2025-01-12 13:36:27 +0800

    remove unused variable

--- src/contact/page.tsx
+++ src/contact/page.tsx
@@ -1,7 +1,4 @@
-import ContactMessage from "./_components/contac-message";
-import ContactDetails from "./_components/contact-details";
 import Contactform from "./_components/contact-form";
-import ContactSubmit from "./_components/contact-submit";
 
 
 
@@ -12,9 +9,6 @@ export default function Contact() {
 
       <div className="flex flex-col min-h-screen max-w-[1200px] lg:mx-auto px-[1.25em]">
 
-        {/* <ContactMessage/> */}
-        {/* <ContactDetails/>
-        <ContactSubmit/> */}
         <Contactform/>
       
       
